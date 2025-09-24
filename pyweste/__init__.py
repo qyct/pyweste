f6b5ece9db5a1305406b74e8c52c7bb36aaad705
qyct <rukmangadan.d@gmail.com>
2025-09-24 14:31:38 +0530

    p

--- pyweste/__init__.py
+++ pyweste/__init__.py
@@ -1,10 +1,3 @@
-"""
-PyWeste - Python Windows Installation Tools
-
-Simple installer interface that reads configuration from pywest.toml
-Usage: __import__("pyweste").installer("path_to_pywest.toml")
-"""
-
 from .installer import installer
 
 __all__ = ["installer"]
